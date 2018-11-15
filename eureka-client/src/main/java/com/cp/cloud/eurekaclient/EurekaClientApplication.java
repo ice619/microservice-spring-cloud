c759a34f039a5cb35be66cabe5dde97ba030f9e8
chenpeng <chenpeng@xiaoniu.com>
2018-11-15 10:51:56 +0800

    断路器监控

--- eureka-client/src/main/java/com/cp/cloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/cp/cloud/eurekaclient/EurekaClientApplication.java
@@ -1,17 +1,34 @@
 package com.cp.cloud.eurekaclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaClientApplication {
+
+	/**
+	 * Hystrix Dashboard
+	 * 访问地址 http://localhost:8762/actuator/hystrix.stream
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
@@ -21,7 +38,23 @@ public class EurekaClientApplication {
 	String port;
 
 	@RequestMapping("/hello")
+	@HystrixCommand(fallbackMethod = "hiError")
 	public String ceshi(@RequestParam(value = "name", defaultValue = "cp") String name){
 		return "hi " + name + " ,i am from port:" + port;
 	}
+
+	public String hiError(String name){
+		return "hi,"+name+",sorry,error!";
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
+
 }
